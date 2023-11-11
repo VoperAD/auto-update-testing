fd467ebaf973bc10a609c94d882dd64465127925
VoperAD <victoraguiarrodrigues@yahoo.com>
2023-11-11 14:52:33 +0100

    chore: initial commit

---
+++ src/main/java/dev/voper/autoupdatetest/AutoUpdateTest.java
@@ -0,0 +1,17 @@
+package dev.voper.autoupdatetest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoUpdateTest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
