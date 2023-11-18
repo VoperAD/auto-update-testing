718fa897b046eee99c47a6c46e24998da16a5793
VoperAD <victoraguiarrodrigues@yahoo.com>
2023-11-18 22:45:38 +0100

    feat: implement the bump script

--- bump-script.js
+++ bump-script.js
@@ -1,3 +1,30 @@
+const { exec } = require('child_process');
+
 exports.preCommit = (props) => {
-    console.log("Hello camaradas")
+    let mvnVersionSet = "mvn versions:set -DnewVersion=" + props.version;
+    let mvnCommit = "mvn versions:commit";
+    let gitCommit = "git commit -am \"chore: bump project version to " + props.version + " [skip ci]\"";
+
+    exec(mvnVersionSet, (err, stdout, stderr) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(stdout);
+        exec(mvnCommit, (err, stdout, stderr) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log(stdout);
+            exec(gitCommit, (err, stdout, stderr) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(stdout);
+            });
+        });
+    });
+
 }
